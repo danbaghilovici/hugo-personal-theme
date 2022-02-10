af04296c6dbc06654a77e457c2774e7aeb95612d
Dan <dan.baghilovici@gmail.com>
2022-02-10 22:09:14 +0200

    add trace to sass for debug

--- styles.bat
+++ styles.bat
@@ -1,1 +1,1 @@
-sass .\modules\supreme-doodle\carbon-for-ibm-dotcom\packages\styles\scss\ibm-dotcom-styles.scss .\assets\css\styles5.css
+sass .\modules\supreme-doodle\carbon-for-ibm-dotcom\packages\styles\scss\ibm-dotcom-styles.scss .\assets\css\styles5.css --trace
