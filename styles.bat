9c676594c0fdec6fd51117307a671f441edb114e
Dan <dan.baghilovici@gmail.com>
2022-02-22 23:20:00 +0200

    update theme

--- styles.bat
+++ styles.bat
@@ -1,1 +1,6 @@
-sass .\modules\supreme-doodle\carbon-for-ibm-dotcom\packages\styles\scss\ibm-dotcom-styles.scss .\assets\css\styles5.css --trace
+:: sass .\modules\supreme-doodle\carbon-for-ibm-dotcom\packages\styles\scss\ibm-dotcom-styles.scss .\assets\css\styles5.css --trace
+
+@REM sass .\modules\supreme-doodle\carbon-custom\theme.scss .\assets\css\styles6.css --trace
+
+sass .\modules\supreme-doodle\carbon\packages\components\src\globals\scss\styles.scss .\assets\css\styles7.css --trace
+
